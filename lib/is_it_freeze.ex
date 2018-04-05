356f65df0c1649c8676835749420affdda1e9597
ecly <ecly@mailbox.org>
2018-04-05 16:35:18 +0200

    implement frozen?/1 and tests for half of 2018

--- lib/is_it_freeze.ex
+++ lib/is_it_freeze.ex
@@ -6,29 +6,60 @@ defmodule IsItFreeze do
   Contexts are also responsible for managing your data, regardless
   if it comes from the database, an external API or others.
   """
+  alias IsItFreeze.BankHolidays
 
-  def is_weekend?(date) do
+  @doc """
+  Returns a boolean indicating whether the given date
+  is a freeze day or not.
+  """
+  def frozen?(date) do
+    is_january_freeze?(date)
+    || is_last_days_in_month_freeze?(date)
+    || is_first_days_in_month_freeze?(date)
+  end
+
+  # TODO - we don't actually freeze entirety
+  # january, so this can be more loose
+  # when we know the exact rules
+  defp is_january_freeze?(date) do
+    {_year, month, _day} = Date.to_erl(date)
+    month == 1
+  end
+
+  defp is_last_days_in_month_freeze?(date) do
+    date
+    |> last_date_in_month()
+    |> Date.range(date)
+    |> Enum.count(&is_bank_working_day?/1)
+    |> :erlang.<(4)
+  end
+
+  defp is_first_days_in_month_freeze?(date) do
+    date
+    |> first_date_in_month()
+    |> Date.range(date)
+    |> Enum.count(&is_bank_working_day?/1)
+    |> :erlang.<(3)
+  end
+
+  defp last_date_in_month(date) do
+    days_in_month = Date.days_in_month(date)
+    {year, month, _day} = Date.to_erl(date)
+    {:ok, last_date} = Date.new(year, month, days_in_month)
+    last_date
+  end
+
+  defp first_date_in_month(date) do
+    {year, month, _day} = Date.to_erl(date)
+    {:ok, first_date} = Date.new(year, month, 1)
+    first_date
+  end
+
+  defp is_weekend?(date) do
     Date.day_of_week(date) in 6..7
   end
 
-  def is_last_2_days?(date) do
-    days_in_month = Date.days_in_month(date)
-    {_year, _month, day} = Date.to_erl(date)
-    days_in_month - day < 2
-  end
-
-  def is_first_3_days?(date) do
-    {_year, _month, day} = Date.to_erl(date)
-    day <= 3
-  end
-
-  def is_bank_day?(date) do
-
-  end
-
-  def frozen?(date) do
-    is_weekend?(date)
-    || is_last_2_days?(date)
-    || is_first_3_days?(date)
+  defp is_bank_working_day?(date) do
+    !is_weekend?(date) && !BankHolidays.is_bank_holiday?(date)
   end
 end
--- test/is_it_freeze/is_it_freeze_test.exs
+++ test/is_it_freeze/is_it_freeze_test.exs
@@ -1,7 +1,6 @@
 defmodule IsItFreezeTest do
   use ExCheck
   use ExUnit.Case, async: true
-  alias ExCheck.Sample
   alias IsItFreeze
 
   @some_date ~D[2000-01-01]
@@ -22,40 +21,116 @@ defmodule IsItFreezeTest do
   end
 
   # Utility, check whether an int will be transformed to
-  # a date that is among the last 2 days of its month
-  def is_last_2_days?(offset) do
+  # a date that is among the last 3 days of its month
+  def is_last_3_days?(offset) do
     date = date_from_int(offset)
     days_in_month = Date.days_in_month(date)
     {_year, _month, day} = Date.to_erl(date)
-    days_in_month - day < 2
+    days_in_month - day < 3
   end
 
   # Utility, check whether an int will be transformed to
-  # a date that is among the first 3 days of its month
-  def is_first_3_days?(offset) do
+  # a date that is among the first 2 days of its month
+  def is_first_2_days?(offset) do
     date = date_from_int(offset)
     {_year, _month, day} = Date.to_erl(date)
-    day <= 3
+    day <= 2
   end
 
-  property :weekend_frozen do
-    for_all x in such_that(xx in int() when is_weekend?(xx)) do
+  property :first_2_days_in_month_frozen do
+    for_all x in such_that(xx in int() when is_first_2_days?(xx)) do
       date = date_from_int(x)
       IsItFreeze.frozen?(date)
     end
   end
 
-  property :first_3_days_in_month_frozen do
-    for_all x in such_that(xx in int() when is_first_3_days?(xx)) do
+  property :last_3_days_in_month_frozen do
+    for_all x in such_that(xx in int() when is_last_3_days?(xx)) do
       date = date_from_int(x)
       IsItFreeze.frozen?(date)
     end
   end
 
-  property :last_2_days_in_month_frozen do
-    for_all x in such_that(xx in int() when is_last_2_days?(xx)) do
-      date = date_from_int(x)
-      IsItFreeze.frozen?(date)
-    end
+  test "2018 first half non-frozen days spotchecking" do
+    # january is temporarily entirely frozen
+    # refute IsItFreeze.frozen?(~D[2018-01-25])
+    refute IsItFreeze.frozen?(~D[2018-02-05])
+    refute IsItFreeze.frozen?(~D[2018-02-09])
+    refute IsItFreeze.frozen?(~D[2018-02-20])
+    refute IsItFreeze.frozen?(~D[2018-02-23])
+    refute IsItFreeze.frozen?(~D[2018-03-05])
+    refute IsItFreeze.frozen?(~D[2018-03-13])
+    refute IsItFreeze.frozen?(~D[2018-03-15])
+    refute IsItFreeze.frozen?(~D[2018-03-23])
+    refute IsItFreeze.frozen?(~D[2018-04-06])
+    refute IsItFreeze.frozen?(~D[2018-04-09])
+    refute IsItFreeze.frozen?(~D[2018-04-16])
+    refute IsItFreeze.frozen?(~D[2018-04-17])
+    refute IsItFreeze.frozen?(~D[2018-04-23])
+    refute IsItFreeze.frozen?(~D[2018-04-24])
+    refute IsItFreeze.frozen?(~D[2018-05-03])
+    refute IsItFreeze.frozen?(~D[2018-05-04])
+    refute IsItFreeze.frozen?(~D[2018-05-15])
+    refute IsItFreeze.frozen?(~D[2018-05-22])
+    refute IsItFreeze.frozen?(~D[2018-05-25])
+    refute IsItFreeze.frozen?(~D[2018-05-28])
+    refute IsItFreeze.frozen?(~D[2018-05-28])
+    refute IsItFreeze.frozen?(~D[2018-06-06])
+    refute IsItFreeze.frozen?(~D[2018-06-11])
+    refute IsItFreeze.frozen?(~D[2018-06-15])
+    refute IsItFreeze.frozen?(~D[2018-06-25])
+    refute IsItFreeze.frozen?(~D[2018-06-26])
+  end
+
+  test "2018 first half frozen days complete" do
+    # freeze period 1
+    assert IsItFreeze.frozen?(~D[2018-01-29])
+    assert IsItFreeze.frozen?(~D[2018-01-30])
+    assert IsItFreeze.frozen?(~D[2018-01-31])
+    assert IsItFreeze.frozen?(~D[2018-02-01])
+    assert IsItFreeze.frozen?(~D[2018-02-02])
+
+    # freeze period 2
+    assert IsItFreeze.frozen?(~D[2018-02-26])
+    assert IsItFreeze.frozen?(~D[2018-02-27])
+    assert IsItFreeze.frozen?(~D[2018-02-28])
+    assert IsItFreeze.frozen?(~D[2018-03-01])
+    assert IsItFreeze.frozen?(~D[2018-03-02])
+
+    # freeze period 3
+    assert IsItFreeze.frozen?(~D[2018-03-26])
+    assert IsItFreeze.frozen?(~D[2018-03-27])
+    assert IsItFreeze.frozen?(~D[2018-03-28])
+    assert IsItFreeze.frozen?(~D[2018-03-29])
+    assert IsItFreeze.frozen?(~D[2018-03-30])
+    assert IsItFreeze.frozen?(~D[2018-03-31])
+    assert IsItFreeze.frozen?(~D[2018-04-01])
+    assert IsItFreeze.frozen?(~D[2018-04-02])
+    assert IsItFreeze.frozen?(~D[2018-04-03])
+    assert IsItFreeze.frozen?(~D[2018-04-04])
+
+    # freeze period 4
+    assert IsItFreeze.frozen?(~D[2018-04-25])
+    assert IsItFreeze.frozen?(~D[2018-04-26])
+    assert IsItFreeze.frozen?(~D[2018-04-27])
+    assert IsItFreeze.frozen?(~D[2018-04-28])
+    assert IsItFreeze.frozen?(~D[2018-04-29])
+    assert IsItFreeze.frozen?(~D[2018-04-30])
+    assert IsItFreeze.frozen?(~D[2018-05-01])
+    assert IsItFreeze.frozen?(~D[2018-05-02])
+
+    # freeze period 5
+    assert IsItFreeze.frozen?(~D[2018-05-29])
+    assert IsItFreeze.frozen?(~D[2018-05-30])
+    assert IsItFreeze.frozen?(~D[2018-05-31])
+    assert IsItFreeze.frozen?(~D[2018-06-01])
+    assert IsItFreeze.frozen?(~D[2018-06-02])
+    assert IsItFreeze.frozen?(~D[2018-06-03])
+
+    # freeze period 6 (june half)
+    assert IsItFreeze.frozen?(~D[2018-06-27])
+    assert IsItFreeze.frozen?(~D[2018-06-28])
+    assert IsItFreeze.frozen?(~D[2018-06-29])
+    assert IsItFreeze.frozen?(~D[2018-06-30])
   end
 end
